c9013e38f8e5455f6551d327a333b8e1773eedd7
Steve Phillips <elimisteve@gmail.com>
2011-01-06 16:09:02 -0800

    Added alphabet.clj, which prints the alphabet in two diff ways. Added filter.clj, a simple demo of the 'filter' function.

---
+++ clj/filter.clj
@@ -0,0 +1,12 @@
+; Steve Phillips / elimisteve
+; 2011.01.06
+
+; Using fn
+(println
+ (filter (fn [x] (> x 3)) (range -10 5)))
+; (4)
+
+; Short-cut
+(println
+ (filter #(> % 3) (range -10 5)))
+; (4)
