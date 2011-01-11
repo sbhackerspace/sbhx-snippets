f937b590201063671f331c4e53bbf83def3e2a4d
gtech <gtech@aeondynamics.com>
2011-01-11 01:44:45 -0800

    Editing quine, I'm so confused by this script

--- clj/quine.clj
+++ clj/quine.clj
@@ -1,12 +1,12 @@
 ; Steve Phillips / elimisteve
 ; 2011.01.11
 
-((fn [x]
-   (list x
-		 (list (quote quote)
-			   x)))
- (quote
-  (fn [x]
-	(list x
-		  (list (quote quote)
-				x)))))
+;;seriously wtf is this?
+((fn [x] (list x
+	       (list (quote quote)
+		     x)))
+ (quote (fn [x] (list x
+		      (list (quote quote)
+			    x)))))
+
+
