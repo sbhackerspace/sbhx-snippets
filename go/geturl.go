9e69e78b29c9bda9918e0a5fc528b2f37ee294de
Steve Phillips <elimisteve@gmail.com>
2011-01-17 11:02:23 -0800

    Added geturl.go, showing how to GET the contents of a webpage

---
+++ go/geturl.go
@@ -0,0 +1,23 @@
+// Steve Phillips / elimisteve
+// 2011.01.16
+
+package main
+
+import (
+	"fmt"
+	"http"
+	"io/ioutil"
+)
+
+func main() {
+	url := "http://decentra.org/md5/string/hash-this-string"
+	req, _, err := http.Get(url)  // _ = final URL retrieved
+	                              // (after redirects)
+	if err == nil {
+		body, _ := ioutil.ReadAll(req.Body);
+		fmt.Printf("%s\n", body)
+		req.Body.Close();
+	} else {
+		fmt.Printf("err: %s\n", err)
+	}
+}
