f9a4251d7f1d6214f9a5246cb15cd984cb93482e
Steve Phillips <elimisteve@gmail.com>
2011-03-21 03:38:25 -0700

    Improving parse-atom.py and the github bot in parallel

--- py/parse-atom.py
+++ py/parse-atom.py
@@ -4,30 +4,33 @@
 
 import feedparser, time
 
-SLEEP_SECONDS = 1
-
 account_name = 'sbhackerspace'
 branch = 'master'
 repo_names = ['sbhx-ircbot', 'sbhx-rov', 'sbhx-sicp', 'sbhx-snippets',
               #'sbhx-androidapp', 'sbhx-projecteuler'
               ## These two don't get parsed correctly for some reason...
               ]
+SLEEP_SECONDS = float(60*2)/len(repo_names)  # Check each repo once/2 minutes
 
 def main():
-    last_updated = {}
+    old_version = {}
     while True:
         for repo in repo_names:
-            old = feedparser.parse('https://github.com/' + account_name +
-                                   '/' + repo + '/commits/' + branch + '.atom')
-            time.sleep(SLEEP_SECONDS)  # Wait then compare
+            old_version[repo] = feedparser.parse(
+                'https://github.com/' + account_name +
+                '/' + repo + '/commits/' + branch + '.atom'
+                )
 
-            d = feedparser.parse('https://github.com/' + account_name +
+        time.sleep(SLEEP_SECONDS)  # Wait then compare
+
+        for repo in repo_names:
+            new = feedparser.parse('https://github.com/' + account_name +
                                    '/' + repo + '/commits/' + branch + '.atom')
 
-            if d.entries[0] != old.entries[0]:
-                #author = d.entries[0].author.split()[0]  # First name
-                author = d.entries[0].author_detail.href.split('/')[-1]
-                commit_msg = d.entries[0].title
+            if new.entries[0] != old_version[repo].entries[0]:
+                #author = new.entries[0].author.split()[0]  # First name
+                author = new.entries[0].author_detail.href.split('/')[-1]
+                commit_msg = new.entries[0].title
                 print author, "committed to", repo
                 print "   ", commit_msg
                 print
