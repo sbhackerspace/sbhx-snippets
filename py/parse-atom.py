6ed76368e741622e856473abb31e4dc829aebc1b
Steve Phillips <elimisteve@gmail.com>
2011-03-21 01:19:14 -0700

    parse-atom.py is getting a lot better. Should now work.

--- py/parse-atom.py
+++ py/parse-atom.py
@@ -6,6 +6,8 @@ import feedparser, time
 
 SLEEP_SECONDS = 1
 
+account_name = 'sbhackerspace'
+branch = 'master'
 repo_names = ['sbhx-ircbot', 'sbhx-rov', 'sbhx-sicp', 'sbhx-snippets',
               #'sbhx-androidapp', 'sbhx-projecteuler'
               ## These two don't get parsed correctly for some reason...
@@ -15,15 +17,19 @@ def main():
     last_updated = {}
     while True:
         for repo in repo_names:
-            old = feedparser.parse('https://github.com/sbhackerspace/' + \
-                                       repo + '/commits/master.atom')
+            old = feedparser.parse('https://github.com/' + account_name +
+                                   '/' + repo + '/commits/' + branch + '.atom')
             time.sleep(SLEEP_SECONDS)  # Wait then compare
-            d = feedparser.parse('https://github.com/sbhackerspace/' + \
-                                         repo + '/commits/master.atom')
+
+            d = feedparser.parse('https://github.com/' + account_name +
+                                   '/' + repo + '/commits/' + branch + '.atom')
 
             if d.entries[0] != old.entries[0]:
-                print d.entries[0].author.split()[0], "committed to", repo
-                print "   ", d.entries[0].title
+                #author = d.entries[0].author.split()[0]  # first name
+                author = d.entries[0].author_detail.href.split('/')[-1]
+                commit_msg = d.entries[0].title
+                print author, "committed to", repo
+                print "   ", commit_msg
                 print
 
 if __name__ == '__main__':
