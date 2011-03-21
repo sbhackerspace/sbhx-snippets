18afde838586a20ffe06845b5c6124f25c0c6b68
Steve Phillips <elimisteve@gmail.com>
2011-03-20 22:38:27 -0700

    Finished RubyKoan 14, about_triangle_project.rb. 11th commit.

--- rubykoans/about_control_statements.rb
+++ rubykoans/about_control_statements.rb
@@ -8,7 +8,7 @@ class AboutControlStatements < EdgeCase::Koan
     else
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_if_then_statements
@@ -16,7 +16,7 @@ class AboutControlStatements < EdgeCase::Koan
     if true
       result = :true_value
     end
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_if_statements_return_values
@@ -25,14 +25,14 @@ class AboutControlStatements < EdgeCase::Koan
             else
               :false_value
             end
-    assert_equal __, value
+    assert_equal :true_value, value
 
     value = if false
               :true_value
             else
               :false_value
             end
-    assert_equal __, value
+    assert_equal :false_value, value
 
     # NOTE: Actually, EVERY statement in Ruby will return a value, not
     # just if statements.
@@ -42,19 +42,19 @@ class AboutControlStatements < EdgeCase::Koan
     value = if false
               :true_value
             end
-    assert_equal __, value
+    assert_equal nil, value
   end
 
   def test_condition_operators
-    assert_equal __, (true ? :true_value : :false_value)
-    assert_equal __, (false ? :true_value : :false_value)
+    assert_equal :true_value, (true ? :true_value : :false_value)
+    assert_equal :false_value, (false ? :true_value : :false_value)
   end
 
   def test_if_statement_modifiers
     result = :default_value
     result = :true_value if true
 
-    assert_equal __, result
+    assert_equal :true_value, result
   end
 
   def test_unless_statement
@@ -62,14 +62,14 @@ class AboutControlStatements < EdgeCase::Koan
     unless false
       result = :false_value
     end
-    assert_equal __, result
+    assert_equal :false_value, result
   end
 
   def test_unless_statement_modifier
     result = :default_value
     result = :false_value unless false
 
-    assert_equal __, result
+    assert_equal :false_value, result
   end
 
   def test_while_statement
@@ -79,7 +79,7 @@ class AboutControlStatements < EdgeCase::Koan
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal 1*2*3*4*5*6*7*8*9*10, result
   end
 
   def test_break_statement
@@ -90,7 +90,7 @@ class AboutControlStatements < EdgeCase::Koan
       result = result * i
       i += 1
     end
-    assert_equal __, result
+    assert_equal 1*2*3*4*5*6*7*8*9*10, result
   end
 
   def test_next_statement
@@ -101,7 +101,7 @@ class AboutControlStatements < EdgeCase::Koan
       next if (i % 2) == 0
       result << i 
     end
-    assert_equal __, result
+    assert_equal [1,3,5,7,9], result
   end
 
   def test_for_statement
@@ -110,7 +110,7 @@ class AboutControlStatements < EdgeCase::Koan
     for item in array
       result << item.upcase
     end
-    assert_equal [__, __, __], result
+    assert_equal ["FISH", "AND", "CHIPS"], result
   end
 
 end
---
+++ rubykoans/triangle.rb
@@ -0,0 +1,29 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+  if a == b and b == c
+    :equilateral
+  elsif
+    (a == b and b != c) or (a == c and c != b) or (b == c and c != a)
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
