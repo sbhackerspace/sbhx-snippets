ffadb7f61cdec5c137991fd0fa0b16c252574895
Steve Phillips <elimisteve@gmail.com>
2011-03-22 20:30:06 -0700

    Finished and added Ruby Koan 17, about_iteration.rb

--- rubykoans/triangle.rb
+++ rubykoans/triangle.rb
@@ -14,6 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  # Side lengths must be positive
+  if !(a > 0 and b > 0 and c > 0)
+    raise TriangleError
+  end
+
+  # Triangle inequality must hold
+  if !(a + b > c and a + c > b and b + c > a)
+    raise TriangleError
+  end
+
   if a == b and b == c
     :equilateral
   elsif
