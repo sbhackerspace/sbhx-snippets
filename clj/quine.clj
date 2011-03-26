a166552f2026ec14125d07f88c8b8c68ed302833
Steve Phillips <elimisteve@gmail.com>
2011-03-25 20:44:53 -0700

    Changed quine.clj back to a quine

--- clj/quine.clj
+++ clj/quine.clj
@@ -1,13 +1,12 @@
 ; Steve Phillips / elimisteve
 ; 2011.01.11
 
-;;seriously wtf is this?
-(defn quine [x]
-  (fn [x](list x
-	       (list (quote quote)
-		     x)))
-  (quote (fn [x] (list x
-		       (list (quote quote)
-			     x)))))
-
-
+((fn [x]
+   (list x
+         (list (quote quote)
+               x)))
+ (quote
+  (fn [x]
+    (list x
+          (list (quote quote)
+                x)))))
