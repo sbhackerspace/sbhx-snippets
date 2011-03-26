abd2d943a32bd2ad9d66a41599beac3dad555091
gtech <gtech@aeondynamics.com>
2011-03-26 15:25:32 -0700

    Added initialization to generator

--- clj/generator.clj
+++ clj/generator.clj
@@ -1,12 +1,16 @@
 ; Steve Phillips / elimisteve
 ; 2011.03.26
 
-(defn generator []
-  (def num 0)
-  (fn []
-    (def num (inc num))
-    num))
-
+(defn generator
+  ([](def num 0)
+     (fn []
+       (def num (inc num))
+       num))
+  ([x] (def num x)
+     (fn []
+       (def num (inc num))
+       num)))
+;;usage
 (def g (generator))
 
 (g)
