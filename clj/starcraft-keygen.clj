d064dba1d7a030e1982cbc5180b3614e8ddd69f6
Steve Phillips <elimisteve@gmail.com>
2011-03-25 21:00:02 -0700

    Simplified starcraft-keygen.clj

--- clj/starcraft-keygen.clj
+++ clj/starcraft-keygen.clj
@@ -2,21 +2,18 @@
 (ns starcraft-keygen)
 
 (defn vector-of-random-digits
-  "Return a vector of random digits. Params: max is the largest possible int from 0 to (n-1), length of vector."
+  "Return a vector of random digits. Params: max is the
+   largest possible int from 0 to (n-1), length of vector."
   [max length]
-  (let [l (dec length)];;Is there a better way to do this? I want to avoid any possibility of infinite loops so I want to use >, but I only want to decriment the upper bound once.
-   (loop [i 0, v []]
-	 (if (> i l)
-	   v
-	   (recur (inc i)
-		  (assoc v i (rand-int max)))))))
-  
+  (loop [i 0, v []]
+    (if (> i length)
+      v
+      (recur (inc i)
+             (assoc v i (rand-int max))))))
+
 
 (defn gen-key
   "Generates and returns a Starcraft key"
   []
   (def first-12-digits
-    (vector-of-random-digits 10 12))
-  
-  
-  
+       (vector-of-random-digits 10 12)))
