f609cddf8dfe2c333da8422e4f845aaae6b80b87
Steve Phillips <elimisteve@gmail.com>
2012-01-17 02:31:33 -0800

    Added very practical example to regexp.go + trick to simulate non-greedy operators

--- go/regexp.go
+++ go/regexp.go
@@ -7,6 +7,7 @@ import (
 	"flag"
 	"fmt"
 	"regexp"
+	"url"
 )
 
 func main() {
@@ -63,5 +64,23 @@ func main() {
 		if digits != nil {
 			fmt.Printf("Digits found: %s\n", answer)
 		}
+	}
+
+	// Finds the desired URL within the larger string
+	html := `flashvars="id_video=1164189&amp;theskin=default&amp;flv_url=http%3A%2F%2Fsite189.website.com%2Fvideos%2Fflv%2F3%2Fa%2Ff%2Fwebsite.com_3af235cd0b24ef5fad91b855001e00ae.flv%3Fe%3D1326724235%26ri%3D1024%26rs%3D85%26h%3D96a70406919dca69c45c4f83ce740ff9&amp;url_bigthumb=http://img100.website.com/videos/thumbs/pic.jpg&amp;`
+
+	// (.*?) isn't a valid regexp in Go, so following is greedy
+	// get_vid_url := regexp.MustCompile("flv_url=(.*)&amp;")
+
+	// Non-greedy trick vaguely hinted at by swtch.com/~rsc/regexp/regexp1.html
+	// get_vid_url := regexp.MustCompile("flv_url=(.*)&amp;.*&amp;")
+
+	// This gets me what I need: the video URL. Won't let me escape the `.`, hmm
+	get_vid_url := regexp.MustCompile("flv_url=(.*.flv)")
+
+	// Looks like [1] will always be what we want; [0] is too broad
+	for ndx, str := range get_vid_url.FindStringSubmatch(html) {
+		decodedURL, _ := url.Parse(str)
+		fmt.Printf("\nFindStringSubmatch[%v]: %v\n", ndx, decodedURL.Path)
 	}
 }
