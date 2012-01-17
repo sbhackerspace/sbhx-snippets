9a4c3a0c958c300001dad36f0455f074ca23c049
Steve Phillips <elimisteve@gmail.com>
2012-01-16 23:15:57 -0800

    Added datetime_diff.py, which calculates the time between now and 1215 tomorrow

---
+++ py/datetime_diff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Steve Phillips / elimisteve
+# 2012.01.16
+
+import datetime
+
+now              = datetime.datetime.now()
+tomorrow_at_1215 = datetime.datetime(now.year, now.month, now.day+1, 12, 15)
+
+diff_in_seconds  = (tomorrow_at_1215 - now).seconds
+diff_in_hours    = diff_in_seconds / 60. / 60. # 60 secs per min, 60 mins per hour
+
+print "Tomorrow at 1215 is %0.2f hours away" % (diff_in_hours,)
+
+hours = int(diff_in_hours)
+mins  = int( 60 * (diff_in_hours - hours) )
+
+print "Also known as %d hours and %d minutes" % (hours, mins)
