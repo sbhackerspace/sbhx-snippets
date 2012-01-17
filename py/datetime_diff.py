a06ec0cebc511430ac9dbef2aeaf8f38cb7f20f2
Steve Phillips <elimisteve@gmail.com>
2012-01-16 23:19:26 -0800

    Slightly clarified datetime_diff.py

--- py/datetime_diff.py
+++ py/datetime_diff.py
@@ -8,11 +8,11 @@ now              = datetime.datetime.now()
 tomorrow_at_1215 = datetime.datetime(now.year, now.month, now.day+1, 12, 15)
 
 diff_in_seconds  = (tomorrow_at_1215 - now).seconds
-diff_in_hours    = diff_in_seconds / 60. / 60. # 60 secs per min, 60 mins per hour
+diff_in_hours    = diff_in_seconds / 60. / 60. # 60 secs/min, 60 mins/hour
 
 print "Tomorrow at 1215 is %0.2f hours away" % (diff_in_hours,)
 
 hours = int(diff_in_hours)
-mins  = int( 60 * (diff_in_hours - hours) )
+mins  = int( 60 * (diff_in_hours - hours) ) # multiply fraction hour by 60
 
 print "Also known as %d hours and %d minutes" % (hours, mins)
