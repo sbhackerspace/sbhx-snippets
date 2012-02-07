efdd15911773a496034ef8a3ed849027d01b3060
Steve Phillips <elimisteve@gmail.com>
2012-02-06 19:36:26 -0800

    type-assertions-doesntwork.go: what not to do. See type-assertions-interfaces.go for the correct way

---
+++ go/type-assertions-doesntwork.go
@@ -0,0 +1,27 @@
+// Steve Phillips / elimisteve
+// 2012.02.06
+
+package main
+
+import (
+	"fmt"
+)
+
+type IntSlice []int
+
+func main() {
+	//
+	// The following is completely wrong. See type-assertions-interfaces.go
+	//
+
+	slice := IntSlice{1, 2, 3}
+	if sl, ok := slice.(IntSlice); ok {
+		fmt.Printf("%v is indeed an IntSlice with value %v\n", slice, ok)
+	}
+
+	number := 5
+	if num, ok := number.(int); ok {
+		fmt.Printf("%v is indeed an int with value %v\n", number, num)
+	}
+
+}
