b283fb8460c9e518a5280be69b8971ed2f996544
Steve Phillips <elimisteve@gmail.com>
2012-02-18 14:16:50 -0800

    Fixed and improved intensity_loop.py

--- py/intensity_loop.py
+++ py/intensity_loop.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # 2012.02.18
 
+import datetime
+
+previous_date = datetime.datetime.now()
+previous_intensity = -1
+
 while True:
     intensity = int(raw_input("Distraction intensity?  "))
     now = datetime.datetime.now()
     diff = (now - previous_date)
-    print "Time elapsed:", diff, "; Previous intensity:", previous_intensity
+    print "Previous intensity:", previous_intensity, "; Elapsed time since:", diff
     previous_date = now
     previous_intensity = intensity
 
