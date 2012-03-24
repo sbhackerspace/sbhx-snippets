d79f4e823d2fde4fad9e5d0371920be1a6cab02e
Steve Phillips <elimisteve@gmail.com>
2012-03-24 14:31:18 -0700

    Smartified repo-renamer.sh

--- bash/repo-renamer.sh
+++ bash/repo-renamer.sh
@@ -3,10 +3,5 @@
 # 2012.03.24
 
 for repo in {django-crud-gen,django-projectbuilder,go-helpers}; do
-    mv proto-$repo/ $repo/
-    cd $repo
-    git remote rm origin
-    git remote add origin git@github.com:prototypemagic/$repo.git
-    git puom
-    cd ..
+    mv proto-$repo/ $repo/ && cd $repo && git remote rm origin && git remote add origin git@github.com:prototypemagic/$repo.git && git puom && cd ..
 done
