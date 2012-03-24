ce5679d8b63d14b0c921b39475a7bb0e74f16d39
Steve Phillips <elimisteve@gmail.com>
2012-03-24 14:14:22 -0700

    Added repo-renamer.sh

---
+++ bash/repo-renamer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Steve Phillips / elimisteve
+# 2012.03.24
+
+for repo in {django-crud-gen,django-projectbuilder,go-helpers}; do
+    mv proto-$repo/ $repo/
+    cd $repo
+    git remote rm origin
+    git remote add origin git@github.com:prototypemagic/$repo.git
+    git puom
+    cd ..
+done
