2c41175d1432fbe0b421d1d37adf3ee14d45f89e
Steve Phillips <elimisteve@gmail.com>
2012-04-20 07:21:10 -0700

    go1/sendMail.go now go1-compatible and tells you who you've emailed

--- go1/sendMail.go
+++ go1/sendMail.go
@@ -5,12 +5,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"net/smtp"
+	"strings"
 )
 
 func main() {
-	const LOGIN = "YOUR_USERNAME@gmail.com"
-	const PASSWORD = "YOUR_PASSWORD"
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: sendMail.go YOUR_USERNAME@gmail.com YOUR_PASSWORD\n")
+		return
+	}
+	LOGIN := os.Args[1]
+	PASSWORD := os.Args[2]
 
 	subject := "Subject: Test Email from Go!"
     content := "Hello from sendMail.go!"
@@ -19,7 +25,10 @@ func main() {
 
     err := smtp.SendMail("smtp.gmail.com:587", auth, LOGIN, recipients,
 		[]byte(subject + "\n\n" + content))
-	if err != nil {
+	if err == nil { 
+		fmt.Printf("Email sent successfully to %v!\n",
+			strings.Join(recipients, ", "))
+	} else {
 		fmt.Printf("Error sending email: %v\n", err)
 	}
 }
