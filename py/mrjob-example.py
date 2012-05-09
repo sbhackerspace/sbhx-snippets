2d2d1fb9cd900143081e6ca6aa1ab02172fdd0ff
Steve Phillips <elimisteve@gmail.com>
2012-05-08 19:07:04 -0700

    Added mrjob-example.py

---
+++ py/mrjob-example.py
@@ -0,0 +1,25 @@
+# From https://github.com/Yelp/mrjob
+
+"""The classic MapReduce job: count the frequency of words.
+"""
+from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+
+class MRWordFreqCount(MRJob):
+
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+
+    def combiner(self, word, counts):
+        yield (word, sum(counts))
+
+    def reducer(self, word, counts):
+        yield (word, sum(counts))
+
+
+if __name__ == '__main__':
+    MRWordFreqCount.run()
