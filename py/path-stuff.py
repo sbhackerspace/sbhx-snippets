c61ff9554c53dc5747ad801dd8233551aaf1c475
Steve Phillips <elimisteve@gmail.com>
2012-05-20 19:41:22 -0700

    Removed unnecessary 'join' from path-stuff.py

--- py/path-stuff.py
+++ py/path-stuff.py
@@ -5,4 +5,4 @@
 import os
 
 print '/'.join(os.path.realpath(__file__).split('/')[:-1])
-print os.path.abspath(os.path.join(os.path.dirname(__file__)))
+print os.path.abspath(os.path.dirname(__file__))
