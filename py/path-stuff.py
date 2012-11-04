d4f008b2ef58309f163747adebb2766736b0c0ef
Steve Phillips <elimisteve@gmail.com>
2012-11-04 03:59:24 -0800

    Added several more examples to py/path-stuff.py

--- py/path-stuff.py
+++ py/path-stuff.py
@@ -4,5 +4,11 @@
 
 import os
 
-print '/'.join(os.path.realpath(__file__).split('/')[:-1])
-print os.path.abspath(os.path.dirname(__file__))
+print 1, '/'.join(os.path.realpath(__file__).split('/')[:-1])
+print 2, os.path.join(os.path.realpath(__file__).split('/')[:-1])
+print 3, os.path.abspath(os.path.dirname(__file__))
+print 4, os.path.realpath(os.path.dirname(__file__)) + '/'
+print 5, os.path.realpath(__file__)
+print 6, os.path.dirname(os.path.realpath(__file__))
+
+#print open(__file__).read()
