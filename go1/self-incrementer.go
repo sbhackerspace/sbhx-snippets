6df15c6903e237e1797313ad24c6ccae181abaf9
Steve Phillips <elimisteve@gmail.com>
2012-11-10 10:14:28 -0800

    self-incrementer.go now uses os.Args[0] as it should. Changed some constant/var names (linePrefix -> LINE_PREFIX) and created constants

--- go1/self-incrementer.go
+++ go1/self-incrementer.go
@@ -13,36 +13,38 @@ import (
 	"strconv"
 )
 
-var incrementMe = 3
+const (
+	LINE_PREFIX       = `var incrementMe = `
+	INCREMENT_COMMAND = `go build self-incrementer.go; ./self-incrementer`
+)
+
+var incrementMe = 0
 
 func main() {
-	linePrefix := `var incrementMe = `
-
-	thisFile := "/home/steve/sbhx/sbhx-snippets/go1/self-incrementer.go"
-	// thisFile := os.Args[0] + ".go"
+	thisFile := os.Args[0] + ".go"
 	contents, err := ioutil.ReadFile(thisFile)
-	fun.MaybeFatalAt("ioutil.ReadFile", err)
+	if err != nil {
+		fmt.Printf("Run this command several times: \n\n    %s\n\n",
+			INCREMENT_COMMAND)
+		return
+	}
 
 	// Find the line where `incrementMe` is defined
-	getNum := regexp.MustCompile(linePrefix + `\d+`)
+	getNum := regexp.MustCompile(LINE_PREFIX + `\d+`)
 	oldLine := getNum.FindAllString(string(contents), -1)[0]
-	// for _, digits := range getNum.FindAllString(string(contents), -1) {
-	// 	fmt.Printf("'%v'\n", digits)
-	// }
 
 	// Parse out the int to be incremented (the current value of `incrementMe`)
-	intStr := oldLine[len(linePrefix):]
-	fmt.Printf("Old value: %v\n", intStr)
+	intStr := oldLine[len(LINE_PREFIX):]
 
 	// Convert to int and increment
 	theInt, err := strconv.Atoi(intStr)
 	fun.MaybeFatalAt("Atoi", err)
 	theInt++
-	fmt.Printf("New value: %v\n", theInt)
+	fmt.Printf("%v\n", theInt)
 
 	// Replace old value of `incrementMe` with new value
 	newContents := bytes.Replace(contents, []byte(oldLine),
-		[]byte(linePrefix + strconv.Itoa(theInt)), 1)
+		[]byte(LINE_PREFIX + strconv.Itoa(theInt)), 1)
 
 	// Open thisFile and set its contents to newContents
 	file, err := os.OpenFile(thisFile, os.O_WRONLY, 0666)
