d6cfa9a7dc4f479295a19878a85c779dcfa39eb0
Steve Phillips <elimisteve@gmail.com>
2013-02-03 17:46:50 -0800

    Added small-u and big-u strings to utf8.go

--- go1/utf8.go
+++ go1/utf8.go
@@ -8,16 +8,26 @@ import (
 )
 
 func main() {
-	msg := "°©®"
-	// Print the same thing, but in hex
+	msg := "°"
+	msg += "©"
+	msg += "®"
+	// Hex strings
 	msg += "\xc2\xb0"
 	msg += "\xc2\xa9"
 	msg += "\xc2\xae"
-	// Again print the same thing, but using small-u Unicode code points
+	// Small-u strings
+	msg += "\u00b0"
+	msg += "\u00a9"
+	msg += "\u00ae"
+	// Big-u strings
+	msg += "\U000000b0"
+	msg += "\U000000a9"
+	msg += "\U000000ae"
+	// Small-u Unicode code points as byte slices
 	msg += string([]byte{'\u00c2', '\u00b0'})
 	msg += string([]byte{'\u00c2', '\u00a9'})
 	msg += string([]byte{'\u00c2', '\u00ae'})
-	// _Again_ print the same thing, but using BIG-u Unicode code points
+	// Big-u Unicode code points
 	msg += string([]byte{'\U000000c2', '\U000000b0'})
 	msg += string([]byte{'\U000000c2', '\U000000a9'})
 	msg += string([]byte{'\U000000c2', '\U000000ae'})
