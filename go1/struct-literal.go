4f4d50f25f72c9f196679b1c1de7d8162f43e8b4
Steve Phillips <elimisteve@gmail.com>
2013-09-25 21:05:28 -0700

    Added struct-literal.go

---
+++ go1/struct-literal.go
@@ -0,0 +1,13 @@
+// Steve Phillips / elimisteve
+// 2013.09.25
+
+package main
+
+func main() {
+	var believeIt = struct {
+		Name string
+	}{
+		Name: "Arthur :-)",
+	}
+	println(believeIt.Name)
+}
