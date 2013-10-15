c4aff12ec0f0909bac5170b8bfb187a166b2b735
AJ Bahnken <aj@ajvb.me>
2013-10-14 18:06:37 -0700

    Added browser version of Javascript fizzbuzz
    (Bored waiting for BeautifulSoup to finish parsing URLs off
    a couple hundred pages...)

--- js/fizzbuzz/fizzbuzz.js
+++ js/fizzbuzz/fizzbuzz.js
@@ -1,8 +1,18 @@
 // @ajvbahnken + @elimisteve
 
+// Printing to the console
 for (var i = 1; i < 101; i++) {
     var line = "";
     if (i % 3 === 0) line += "Fizz";
     if (i % 5 === 0) line += "Buzz";
     console.log(line || i);
+}
+
+// Printing to the browser
+var d = document.getElementsByTagName('div')[0];
+for (var i = 1; i < 101; i++) {
+    var line = "";
+    if (i % 3 === 0) line += "Fizz";
+    if (i % 5 === 0) line += "Buzz";
+    d.innerHTML += (line || i) + '<br>';
 }
