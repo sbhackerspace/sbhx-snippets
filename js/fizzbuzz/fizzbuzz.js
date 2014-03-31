2219ba2a9a2294f744025efce56368a16f6efa77
AJ Bahnken <aj@ajvb.me>
2014-03-31 09:18:05 -0700

    <= not =< in fizzbuzz.js

--- js/fizzbuzz/fizzbuzz.js
+++ js/fizzbuzz/fizzbuzz.js
@@ -1,7 +1,7 @@
 // @ajvbahnken + @elimisteve
 
 // Printing to the console
-for (var i = 1; i =< 100; i++) {
+for (var i = 1; i <= 100; i++) {
     var line = "";
     if (i % 3 === 0) line += "Fizz";
     if (i % 5 === 0) line += "Buzz";
@@ -10,7 +10,7 @@ for (var i = 1; i =< 100; i++) {
 
 // Printing to the browser
 var d = document.getElementsByTagName('div')[0];
-for (var i = 1; i =< 100; i++) {
+for (var i = 1; i <= 100; i++) {
     var line = "";
     if (i % 3 === 0) line += "Fizz";
     if (i % 5 === 0) line += "Buzz";
