7cabadf5bd6a9e425edde28d59e8df1cada23bfb
Steve Phillips <elimisteve@gmail.com>
2014-05-04 01:17:49 -0700

    clone-sbhx.sh: Added sbhx-ntlanman

--- bash/clone-sbhx.sh
+++ bash/clone-sbhx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~; mkdir sbhx; cd sbhx; for repo in {ai,androidapp,boardinator,cryptag,firefoxos,gomandrill,ircbot,projecteuler,rov,rubyquiz,sicp,signin,sbitter,snippets,spaceapi,the-fist,website,webtech}; do git clone git@github.com:sbhackerspace/sbhx-$repo; echo; echo; done
+cd ~; mkdir sbhx; cd sbhx; for repo in {ai,androidapp,boardinator,cryptag,firefoxos,gomandrill,ircbot,ntlanman,projecteuler,rov,rubyquiz,sicp,signin,sbitter,snippets,spaceapi,the-fist,website,webtech}; do git clone git@github.com:sbhackerspace/sbhx-$repo; echo; echo; done
