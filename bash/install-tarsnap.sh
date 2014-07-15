2c32695995d810c4591bd82197029113d8d6a892
Steve Phillips <steve@tryingtobeawesome.com>
2014-07-15 04:33:26 -0700

    install-tarsnap.sh: Added $1 version parameter

--- bash/install-tarsnap.sh
+++ bash/install-tarsnap.sh
@@ -2,18 +2,25 @@
 # Steve Phillips / elimisteve
 # 2014.07.14
 
-wget https://www.tarsnap.com/download/tarsnap-autoconf-1.0.35.tgz
-wget https://www.tarsnap.com/download/tarsnap-sigs-1.0.35.asc
+if [ $# -lt 1 ]; then
+    echo "`basename $0` version (e.g., 1.0.35)"
+    exit 1
+fi
+
+version=$1
+
+wget https://www.tarsnap.com/download/tarsnap-autoconf-${version}.tgz
+wget https://www.tarsnap.com/download/tarsnap-sigs-${version}.asc
 wget https://www.tarsnap.com/tarsnap-signing-key.asc
 
 sudo apt-get update
 sudo apt-get install hashalot libssl-dev zlib1g-dev e2fslibs-dev
 
 gpg --import < tarsnap-signing-key.asc
-gpg --decrypt tarsnap-sigs-1.0.35.asc
-sha256 tarsnap-autoconf-1.0.35.tgz
-tar xvf tarsnap-autoconf-1.0.35.tgz 
-cd tarsnap-autoconf-1.0.35/
+gpg --decrypt tarsnap-sigs-${version}.asc
+sha256 tarsnap-autoconf-${version}.tgz
+tar xvf tarsnap-autoconf-${version}.tgz
+cd tarsnap-autoconf-${version}/
 ./configure 
 make all install clean
 sudo make all install clean
