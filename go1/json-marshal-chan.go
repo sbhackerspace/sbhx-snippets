1e977ca61e65ca5ce88d492501dd7d47532a7463
Steve Phillips <steve@tryingtobeawesome.com>
2014-12-07 22:28:43 -0800

    json-marshal-chan.go: WillMarshal: Added {Recv,Send}Int methods

--- go1/json-marshal-chan.go
+++ go1/json-marshal-chan.go
@@ -16,12 +16,16 @@ type WillMarshal struct {
 	ints chan int
 }
 
-func (will *WillMarshal) Ints() int {
+func (will *WillMarshal) RecvInt() int {
 	return <-will.ints
 }
 
+func (will *WillMarshal) SendInt(n int) {
+	will.ints <- n
+}
+
 func main() {
-	// Will marshal
+	// Will marshal because all exported fields are JSON-marshalable
 	will := &WillMarshal{ints: make(chan int)}
 	jsonData, _ := json.Marshal(will)
 	log.Printf("Successfully marshaled %T: %s\n", will, jsonData)
