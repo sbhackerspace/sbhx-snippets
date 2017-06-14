06baec8933b9b057822e9e7d82bbddadf89f30e6
Steve Phillips <steve@tryingtobeawesome.com>
2017-06-14 04:00:53 -0700

    Added scrape.clj, which scrapes a webpage and counts how many times a regex pattern matches the body

---
+++ clj/scrape.clj
@@ -0,0 +1,12 @@
+;; Steve Phillips / elimisteve
+;; 2017.06.14
+
+(require '[org.httpkit.client :as http])
+
+(let [{:keys [status headers body error] :as resp} @(http/get "https://clojure.github.io/clojure/clojure.core-api.html")]
+  (if error
+    (println "Failed, exception: " error)
+    (let [h2s (map second (re-seq #"<h2 id=\"(?:.*?)\">(.*?)</h2>" body))]
+      (printf "There are %d keywords in the latest version of Clojure: %s"
+              (count h2s)
+              (string/join " " h2s)))))
