033d7ce93b1645605f6ee5edcc17e1e1279f8aee
Steve Phillips <steve@tryingtobeawesome.com>
2020-10-05 21:34:40 -0700

    py/clj-funcnames.py: Updated to support new URL and HTML structure

--- py/clj-funcnames.py
+++ py/clj-funcnames.py
@@ -2,11 +2,12 @@
 # Steve Phillips / elimisteve
 # 2011.01.06
 
-import re, urllib2
+import re
+import urllib2
 
-html = urllib2.urlopen('http://richhickey.github.com/clojure/clojure.core-api.html').read()
+html = urllib2.urlopen('https://clojure.github.io/clojure/clojure.core-api.html').read()
 html = html.replace('&gt;', '>').replace('&lt;', '<')
 
-for func in re.findall(r"<a href=\"#clojure.core/(.*?)\">", html, re.DOTALL):
+for func in re.findall(r'<a href="#clojure.core/(.*?)" class="toc-entry-anchor">', html, re.DOTALL):
     print func
 
